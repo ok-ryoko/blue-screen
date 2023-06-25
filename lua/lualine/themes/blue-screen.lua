942c38e8ac7cae9c9c9177e44326143ffbab90b3
OK Ryoko <ryoko@kyomu.jp.net>
2023-06-25 13:09:50 -0400

    hide inactive status lines

--- lua/lualine/themes/blue-screen.lua
+++ lua/lualine/themes/blue-screen.lua
@@ -6,4 +6,9 @@ return {
     b = { fg = p.blue6, bg = p.blue1 },
     c = { fg = p.blue6, bg = p.blue1 },
   },
+  inactive = {
+    a = { fg = p.blue0, bg = p.blue4 },
+    b = { fg = p.blue4, bg = p.blue0 },
+    c = { fg = p.blue4, bg = p.blue0 },
+  },
 }
